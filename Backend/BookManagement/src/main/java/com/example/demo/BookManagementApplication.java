2b40b706220a0ea36d6b454d753d2b52abaf6973
Jeevani <jeevaniveeramachaneni8@gmail.com>
2025-10-14 15:04:20 +0530

    Updated

--- Backend/BookManagement/src/main/java/com/example/demo/BookManagementApplication.java
+++ Backend/BookManagement/src/main/java/com/example/demo/BookManagementApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookManagementApplication.class, args);
+		SpringApplication.run(BookManagementApplication.class, args); 
 	}
 
 }
